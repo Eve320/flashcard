efce7468730062ace20a66e6f7499b22e3e5649c
Eve320 <enduku98@gmail.com>
2020-06-11 11:06:37 -0700

    adding content

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,54 @@ $(document).ready(function() {
     $("#card1 .card-text").toggle();
   });
 
+  $("#card2").click(function() {
+    $("#card2 .card-text").toggle();
+  });
+
+
+  $("#card3").click(function() {
+    $("#card3 .card-text").toggle();
+  });
+
+
+  $("#card4").click(function() {
+    $("#card4 .card-text").toggle();
+  });
+
+
+  $("#card5").click(function() {
+    $("#card5 .card-text").toggle();
+  });
+
+
+  $("#card6").click(function() {
+    $("#card6 .card-text").toggle();
+  });
+
+  $("#card7").click(function() {
+    $("#card7 .card-text").toggle();
+  });
+
+  $("#card8").click(function() {
+    $("#card8 .card-text").toggle();
+  });
+
+  $("#card9").click(function() {
+    $("#card9 .card-text").toggle();
+  });
+
+  $("#card10").click(function() {
+    $("#card10 .card-text").toggle();
+  });
+
+  $("#card11").click(function() {
+    $("#card11 .card-text").toggle();
+  });
+
+  $("#card12").click(function() {
+    $("#card12 .card-text").toggle();
+  });
+
+
+
 });
